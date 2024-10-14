ab405d2a4a03a6cf991c1665a05c777f7edeca89
Saurish <saurishsum10@outlook.com>
2024-10-14 10:34:09 -0700

    complete part 1 and 2

--- hw1.py
+++ hw1.py
@@ -3,12 +3,27 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
+def vowel_count(val:str) -> int:
+    l = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for i in val:
+        if i.lower() in l:
+            count += 1
 
+    return count
 
 # Part 2
+def short_lists(val:list[list[int]]) -> list[list[int]]:
+    nList = []
 
+    for i in val:
+        if len(i) == 2:
+            nList.append(i)
+
+    return nList
 
 # Part 3
+
 
 
 # Part 4
